a0823a57c2c99f33665ce3fbdacb4b76b453bc33
Israel <israelmorantl@gmail.com>
2026-01-26 23:25:13 -0600

    Corrigiendo nombre de carpetas

--- Practica 2 V2/Practica 2 V2/Practica2/src/Practica2/Cliente.java
+++ Practica 2 V2/Practica 2 V2/Practica2/src/Practica2/Cliente.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package proyecto_final_banco;
+package Practica2;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -27,7 +27,7 @@ public class Cliente {
             Scanner sn = new Scanner(System.in);
             
             sn.useDelimiter("\n");
-            String hostt="192.168.1.109";//Ip del la pc que vaya ser el servidor
+            String hostt="127.0.0.1";//Ip del la pc que vaya ser el servidor
             int portt=10000;//Tanto el cliente como el servidor deben de tener el mismo puerto
             Socket sc = new Socket(hostt,portt);//La ip es la del PC que hace de servidor
             //Tambien el port debe ser el mismo tanto en el cliente como en la clase servidor
